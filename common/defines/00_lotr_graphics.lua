9196b13d26a141800215827a640fb11b19758cd4
Qelifern <amadeus.lundell@gmail.com>
2025-08-05 23:45:01 +0200

    LOTRMOD

---
+++ common/defines/00_lotr_graphics.lua
@@ -0,0 +1,34 @@
+
+NDefines.NGraphics.DRAW_REFRACTIONS_CUTOFF = 0
+NDefines.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {230, 320, 500}  -- At what camera distance the VP name text disappears.
+NDefines.NGraphics.UNITS_DISTANCE_CUTOFF = 150
+
+NDefines.NGraphics.SUN_HEIGHT  = 850 -- higher means softer shadows and more intense light
+NDefines.NGraphics.SUN_DIFFUSE_COLOR   	= {0.14, 0.3, 1.0}
+NDefines.NGraphics.MOON_INTENSITY 			= 1.0
+
+NDefines.NFrontend.FRONTEND_POS_X = 1558.0
+NDefines.NFrontend.FRONTEND_POS_Y = 1900.0
+NDefines.NFrontend.FRONTEND_POS_Z = 1100.0
+NDefines.NFrontend.CAMERA_MIN_HEIGHT = 40.0
+
+NDefines.NGraphics.TRAIT_LINE_ASSIGNED_COLOR = { 1.0, 1.0, 0.0 } -- Color for parent dependency lines when the parent is assigned.
+NDefines.NGraphics.TRAIT_LINE_NON_ASSIGNED_COLOR = { 0.6, 0.6, 0.6 } -- Color for parent dependency lines when the parent is not assigned assigned.
+NDefines.NGraphics.TRAIT_LINE_HIGHLIGHT_COLOR = { 1.0, 1.0, 1.0 } -- Color for parent dependency lines to the parents when hovering over a trait.
+NDefines.NGraphics.TRAIT_INVALID_FOR_ASSIGNMENT_COLOR = { 0.8, 0.3, 0.3 }
+
+-- Onmap icon grouping
+NDefines.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 90.0 -- camera distance at which the icons begin to group up
+NDefines.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 350.0 -- Camera distance at which the icons begin to group up on state level
+NDefines.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 500 -- second camera distance at which the icons begin to group up
+NDefines.NGraphics.UNITS_ICONS_DISTANCE_CUTOFF = 1000
+NDefines.NGraphics.MAP_ICONS_STRATEGIC_AREA_HUGE = 300
+NDefines.NGraphics.MAP_ICONS_STATE_HUGE = 120
+NDefines.NGraphics.MAP_ICONS_GROUP_SPLIT_SELECTED_LIMIT = 5   -- Maximum number of units selected that will cause icon stacks to split
+NDefines.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 500 -- Distance at which icon grouping becomes very coarse and merges different types of units
+NDefines.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 500 -- Distance at which icon grouping becomes very coarse and merges different types of units for strategic mapmodes
+
+NDefines.NGraphics.FRIEND_COLOR  = {1.0, 1.0, 1.0}
+NDefines.NGraphics.ENEMY_COLOR   = {1.0, 0.7, 0.7}
+NDefines.NGraphics.NEUTRAL_COLOR = {0.8, 0.8, 0.8}
+
